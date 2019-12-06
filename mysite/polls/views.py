55603dc0f2f23d5de6e1adde05fc98b359364a4e
datapsyche <jithinjkumar@gmail.com>
2019-12-06 12:26:12 +0530

    added django_tempaltes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,21 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World, This is from polls app")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-def about(request):
-    return HttpResponse("This is About Page")
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def result(request):
+    return HttpResponse("This is result page")
+
+def vote(request):
+    return HttpResponse("This is vote page")
+
